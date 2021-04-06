dd10449b31a0f9db54c708a1cb49add4463ac407
Robert Mannifield <rmannifield@gmail.com>
2021-04-06 18:50:13 -0400

    Part 1

--- src/main/java/com/RUSpark/RedditPhotoImpact.java
+++ src/main/java/com/RUSpark/RedditPhotoImpact.java
@@ -2,11 +2,13 @@ package com.RUSpark;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import java.util.Comparator;
 
 import scala.Tuple2;
 
@@ -16,30 +18,40 @@ public class RedditPhotoImpact {
 
 	public static void main(String[] args) throws Exception {
 
-    if (args.length < 1) {
-      System.err.println("Usage: RedditPhotoImpact <file>");
-      System.exit(1);
-    }
-		
+		if (args.length < 1) {
+			System.err.println("Usage: RedditPhotoImpact <file>");
+			System.exit(1);
+		}
+
 		String InputPath = args[0];
-		
+
 		/* Implement Here */ 
 		SparkSession spark = SparkSession.builder().appName("RedditPhotoImpact").getOrCreate();
-		
+
 		JavaRDD<String> lines = spark.read().textFile(InputPath).javaRDD();
-		
-		JavaPairRDD<String, Integer> parseCount = lines.mapToPair(s -> {  
-																String[] tempStr = s.split(",");
-																Integer sum = Integer.parseInt(tempStr[4]) + Integer.parseInt(tempStr[5]) + Integer.parseInt(tempStr[6]);
-																return new Tuple2<>(tempStr[0], sum);
-															});
-		JavaPairRDD<String, Integer> counts = parseCount.reduceByKey((i, j) -> i + j);
-	    List<Tuple2<String, Integer>> output = counts.collect();
-	    for (Tuple2<?,?> tuple : output) {
-	        System.out.println(tuple._1() + " " + tuple._2());
-	      }
-	  		
-	      spark.stop();
+
+		JavaPairRDD<Integer, Integer> parseCount = lines.mapToPair(s -> {  
+			String[] tempStr = s.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);	
+			System.out.println("STR: " 
+						+ tempStr[0] + ", with vals: " 
+						+ tempStr[4] + ", " 
+						+ tempStr[5] + ", " 
+						+ tempStr[6]);
+			Integer sum = Integer.parseInt(tempStr[4]) + Integer.parseInt(tempStr[5]) + Integer.parseInt(tempStr[6]);
+			return new Tuple2<>(Integer.parseInt(tempStr[0]), sum);
+		});
+
+		JavaPairRDD<Integer, Integer> counts = parseCount.reduceByKey((i, j) -> i + j);
+
+		ArrayList<Tuple2<Integer, Integer>> output = new ArrayList<Tuple2<Integer, Integer>>(counts.collect());
+
+		output.sort(Comparator.comparing(t -> t._1()));
+
+		for (Tuple2<?,?> tuple : output) {
+			System.out.println(tuple._1() + " " + tuple._2());
+		}
+
+		spark.stop();
 	}
 
 }
