55f62177deccef8cb49f4def059a5c97e6cac4df
Robert Mannifield <rmannifield@gmail.com>
2021-04-10 14:52:31 -0400

    part4v1

--- src/main/java/com/RUSpark/NetflixGraphGenerate.java
+++ src/main/java/com/RUSpark/NetflixGraphGenerate.java
@@ -1,5 +1,14 @@
 package com.RUSpark;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.SparkSession;
+
+import scala.Tuple2;
+
 /* any necessary Java packages here */
 
 public class NetflixGraphGenerate {
@@ -14,6 +23,29 @@ public class NetflixGraphGenerate {
 		String InputPath = args[0];
 		
 		/* Implement Here */ 
+		SparkSession spark = SparkSession.builder().appName("NetflixMovieAverage").getOrCreate();
+		
+		JavaRDD<String> lines = spark.read().textFile(InputPath).javaRDD();
+		
+		JavaPairRDD<Tuple2<Integer, Integer>, Integer> pairs = lines.mapToPair(s -> {
+			String[] tempStr = s.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+			return new Tuple2<>(new Tuple2<Integer, Integer>(Integer.valueOf(tempStr[0]), Integer.valueOf(tempStr[2])), Integer.valueOf(tempStr[1]));
+		});
+		
+		JavaPairRDD<Tuple2<Tuple2<Integer,Integer>,Integer>,Tuple2<Tuple2<Integer,Integer>,Integer>> pairs2 = pairs.cartesian(pairs)
+				.filter((Tuple2<Tuple2<Tuple2<Integer,Integer>,Integer>,Tuple2<Tuple2<Integer,Integer>,Integer>> a) -> a._1()._2() < a._2()._2());
+		
+		JavaPairRDD<Tuple2<Integer, Integer>, Integer> ones = pairs2.mapToPair(p -> new Tuple2<>(new Tuple2<>(p._1()._2(), p._2()._2()), 1));
+		
+		JavaPairRDD<Tuple2<Integer, Integer>, Integer> counts = ones.reduceByKey((i,j) -> i + j);
+		
+		ArrayList<Tuple2<Tuple2<Integer, Integer>, Integer>> output = new ArrayList<>(counts.collect());
+		
+		output.sort(Comparator.comparing(t -> t._1()._1()));
+		
+		for (Tuple2<Tuple2<Integer, Integer>, Integer> t : output) {
+			System.out.println(t._1()._1() + " " + t._1()._2() + " " + t._2());
+		}
 		
 	}
 
